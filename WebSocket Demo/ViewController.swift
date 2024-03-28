e9907c3baa92f72f637a03de453da1de339deeb1
omrobbie <putu.roby@yahoo.com>
2024-03-28 11:29:48 +0700

    prepare UI

--- WebSocket Demo/ViewController.swift
+++ WebSocket Demo/ViewController.swift
@@ -8,7 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var messageTextView: UITextView!
+    @IBOutlet weak var connectionButton: UIButton!
+
+    private var isConnected: Bool = false {
+        didSet {
+            statusLabel.text = isConnected ? "Connected" : "Disconnected"
+            statusLabel.backgroundColor = isConnected ? .yellow : .red
+
+            connectionButton.setTitle(
+                isConnected ? "Connect" : "Disconnect",
+                for: .normal
+            )
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    private func webSocketConnect() {
+        isConnected = true
+    }
+
+    private func webSocketDisconnect() {
+        isConnected = false
+    }
+
+    @IBAction func didConnectionButtonTapped(_ sender: Any) {
+        if isConnected {
+            webSocketDisconnect()
+        } else {
+            webSocketConnect()
+        }
     }
 }
